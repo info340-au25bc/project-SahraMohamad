9f17ef44ad7408a7e1ce034bbe5d73f121b6e1c1
echung123 <152647432+echung123@users.noreply.github.com>
2025-11-15 16:15:21 -0800

    Update Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+ return (
+   <footer style={{
+     backgroundColor: 'white',
+     padding: '20px',
+     textAlign: 'center',
+     marginTop: '40px',
+     borderTop: '1px solid #eadfcd'
+   }}>
+     <p>&copy; 2025 Meal Planner App | Created by Sahra Mohamad, Eunseo Chung</p>
+   </footer>
+ );
+}
